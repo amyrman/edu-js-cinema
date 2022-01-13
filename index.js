b826e43a1dc43de113d105aae0754fb787fc22ea
Emil Noren <stratosfloor@hotmail.com>
2022-01-13 12:22:13 +0100

    Added express routes for / and main.css

--- index.js
+++ index.js
@@ -5,4 +5,16 @@ import fs from "fs/promises";
 const app = express();
 
 
+app.get('/', async (request, response) => {
+  const fileBuf = await fs.readFile('./index.html');
+  response.type('html');
+  response.send(fileBuf);
+})
+
+app.get('*.css', async (request, response) => {
+  const fileBuf = await fs.readFile('./main.css');
+  response.type('css');
+  response.send(fileBuf)
+})
+
 app.listen(5080);
