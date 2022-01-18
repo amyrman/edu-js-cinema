de3cb5c0ddbca5aca32687fef9d66745aa91b452
Emil Noren <stratosfloor@hotmail.com>
2022-01-18 14:19:08 +0100

    Set up minimal working website with express-handlebars

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 import express from "express";
-import fs from "fs/promises";
+import { engine } from "express-handlebars";
 
 
 const app = express();
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './templates')
+
 
 app.get('/', async (request, response) => {
-  const fileBuf = await fs.readFile('./index.html');
-  response.type('html');
-  response.send(fileBuf);
+  response.render('index');
 })
 
 
-app.get('/*', async (request, response) => {
-  try {
-    const fileName = request.path;
-    const fileBuf = await fs.readFile(`./${fileName}`);
-    const type = fileName.split('.')[1];
-    response.type(type);
-    response.send(fileBuf);
-  }
-  catch (err) {
-    response.status(404).end();
-  }
-});
-
-// app.use(express.static('./'));
+app.use('/', express.static('./static'));
+// app.use('/', express.static('./static/img'));
 
 app.listen(5080);
