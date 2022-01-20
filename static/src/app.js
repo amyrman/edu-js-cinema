f1ed4c75ac5c3a7c6914e5a5d86a7292959fbdf8
Emil Noren <stratosfloor@hotmail.com>
2022-01-20 11:11:55 +0100

    Moved entire app to separate file except app.listen-function

---
+++ static/src/app.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { engine } from "express-handlebars";
+import { loadAllMovies, loadMovie } from "./movies.js";
+import { kino } from "./kinoBuilds.js";
+import { marked } from "marked";
+
+const app = express();
+
+app.engine("handlebars", engine({
+  helpers: {
+    markdown: md => marked(md),
+  },
+}));
+app.set("view engine", "handlebars");
+app.set("views", "./templates");
+
+
+app.get("/", async (request, response) => {
+  response.render("index", { kino });
+});
+app.get("/index", async (request, response) => {
+  response.render("index", { kino });
+});
+
+app.get("/movies", async (request, response) => {
+  const movies = await loadAllMovies();
+  response.render("allMovies", {movies, kino});
+});
+
+app.get("/movies/:Id", async (request, response) => {
+  const movie = await loadMovie(request.params.Id);
+  movie
+    ? response.render("movie", { movie, kino })
+    : response.status(404).render("404", { kino });
+});
+
+app.use("/", express.static("./static"));
+
+export default app;
