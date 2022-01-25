d941ef2ad7c611a05794b01717c0d72cdb6fe63f
Chrikelin <Christer.resin@gmail.com>
2022-01-25 19:26:51 +0100

    cut out logic for screenings filter and sort. replaced with server logic and get method to /screenings

--- static/script.js
+++ static/script.js
@@ -1,83 +1,4 @@
-
 console.log("Running..");
-
-// Load screenings from API
-const API_BASEURL =
-  'https://lernia-kino-cms.herokuapp.com/api/screenings?populate=movie';
-
-const trimData = (screening) => {
-  return {
-    id: screening.id,
-    title: screening.attributes.movie.data.attributes.title,
-    movieId: screening.attributes.movie.data.id,
-    image: screening.attributes.movie.data.attributes.image.url,
-    ...screening.attributes,
-  };
-};
-
-const filterOldScreenings = (screenings, todaysDate) => {
-  const upcomingScreenings = screenings.filter((screening) => {
-    if (Date.parse(screening.start_time) > Date.parse(todaysDate)) {
-      return screening;
-    }
-  });
-  return upcomingScreenings;
-};
-
-const filterNextFiveDaysScreenings = (screenings, futureDate) => {
-  const filteredScreenings = screenings.filter((screening) => {
-    if (Date.parse(screening.start_time) < Date.parse(futureDate)) {
-      return screening;
-    }
-  });
-  return filteredScreenings;
-};
-
-const sortScreeningsByDate = (screenings) => {
-  const sortedScreenings = screenings.sort((a, b) => {
-    const keyA = new Date(a.start_time);
-    const keyB = new Date(b.start_time);
-    // Comparing the two dates
-    if (keyA < keyB) {
-      return -1;
-    }
-    if (keyA > keyB) {
-      return 1;
-    }
-    return 0;
-  });
-
-  return sortedScreenings;
-};
-
-const filterByUpomingScreenings = (arr, days) => {
-  const todaysDate = new Date();
-  const ms = new Date().getTime() + 86400000 * days;
-  const futureDate = new Date(ms);
-
-  const filteredByOldScreenings = filterOldScreenings(arr, todaysDate);
-
-  const filteredByFutureScreenings = filterNextFiveDaysScreenings(
-    filteredByOldScreenings,
-    futureDate
-  );
-
-  const sortedAndFilteredScreenings = sortScreeningsByDate(
-    filteredByFutureScreenings
-  );
-
-  return sortedAndFilteredScreenings.splice(0, 10);
-};
-
-const getUpcomingScreenings = async () => {
-  const dataBuff = await fetch(API_BASEURL);
-  const data = await dataBuff.json();
-  const screeningsData = await data.data;
-
-
-  const trimedData = screeningsData.map((screening) => trimData(screening));
-  return filterByUpomingScreenings(trimedData, 5);
-};
 
 // Load movies data from API/DB
 const load = async() => {
@@ -110,7 +31,7 @@ const loadScreenings = async () => {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data.data)
+    return data.data;
   } catch (error) {
     console.log(error)
   }
@@ -155,9 +76,7 @@ const createScreeningCards = (screening, screeningsListEl) => {
 const render = async () => {
   const data = await load()
   const eventsData = await loadEvents()
-
-  const screeningsData = await getUpcomingScreenings();
-  console.log(screeningsData);
+  const screeningsData = await loadScreenings();
 
   // console.log('EVENTS LOADED')
   // console.log(eventsData)
