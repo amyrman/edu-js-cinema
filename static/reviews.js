9976a07ac9cfa53283ff7518ca8341fc0b2da8d2
Emil Noren <stratosfloor@hotmail.com>
2022-02-03 13:06:57 +0100

    Refactor rendering reviews

--- static/reviews.js
+++ static/reviews.js
@@ -1,15 +1,30 @@
+window.onload = async ( )=> {
+  try { 
+    renderReviews();
+  } catch { (error)
+    console.log(error);
+  }
+}
+
+const getReviews = async () => {
+  const movieId = window.location.pathname;
+  const response = await fetch(`/api${movieId}/reviews`);
+  const payload = await response.json();
+  return payload; 
+}
 
 const renderReviews = async () => {
   
   // Fetch reviews for movie
-  const movieId = window.location.pathname;
-  const response = await fetch(`/api${movieId}/reviews`);
-  const payload = await response.json();
-  console.log('Antal: ' + payload.data.length)
-
+  // const movieId = window.location.pathname;
+  // const response = await fetch(`/api${movieId}/reviews`);
+  // const payload = await response.json();
+  // console.log('Antal: ' + payload.data.length)
+  const reviews = await getReviews();
+  
   // Calculate and set parameters for reviewpages
   const reviewsPerPage = 5;
-  const reviewsPages = Math.ceil(payload.data.length / reviewsPerPage);
+  const reviewsPages = Math.ceil(reviews.data.length / reviewsPerPage);
   let currentPage = 1;
   
   // Render the reviews for page nr #
@@ -20,18 +35,18 @@ const renderReviews = async () => {
     for(let i = (pageNumber-1) * reviewsPerPage; i < reviewsPerPage*pageNumber; i++) {
       
       // Create reviews in DOM
-      if (payload.data[i]) {
+      if (reviews.data[i]) {
         const comment = document.createElement('span');
         comment.classList.add('reviewComment');
-        comment.innerText = payload.data[i].comment;
+        comment.innerText = reviews.data[i].comment;
       
         const rating = document.createElement('span');
         rating.classList.add('reviewRating');
-        rating.innerText = 'Betyg: ' + payload.data[i].rating;
+        rating.innerText = 'Betyg: ' + reviews.data[i].rating;
     
         const author = document.createElement('blockquote');
         author.classList.add('reviewAuthor');
-        author.innerText = payload.data[i].author;
+        author.innerText = reviews.data[i].author;
     
         const li = document.createElement('li');
         li.append(comment);
@@ -55,6 +70,7 @@ const renderReviews = async () => {
       nextPage.classList.add('nextPage');
       nextPage.innerHTML = '&#10095;';
   
+      
       prevPage.addEventListener("click", () => {
         if (currentPage == 1) {
           currentPage = reviewsPages;
@@ -72,19 +88,15 @@ const renderReviews = async () => {
         }
         renderReviewPage(currentPage);
       })
+
       const showPageNumbers = document.createElement('span');
       showPageNumbers.innerText = `${currentPage}/${reviewsPages}`;
 
-  
       pages.append(prevPage);
       pages.append(showPageNumbers);
       pages.append(nextPage);
       document.querySelector('#reviewsList').append(pages);
-
     }
   }
-
   renderReviewPage(currentPage);
 }
-
-renderReviews();
