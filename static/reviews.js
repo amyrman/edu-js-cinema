aa18ee41b1b6d0780dbf7adf9bed6139d4785ece
Emil Noren <stratosfloor@hotmail.com>
2022-02-03 13:21:48 +0100

    Refactoring functions in for reviews

--- static/reviews.js
+++ static/reviews.js
@@ -14,89 +14,81 @@ const getReviews = async () => {
 }
 
 const renderReviews = async () => {
-  
-  // Fetch reviews for movie
-  // const movieId = window.location.pathname;
-  // const response = await fetch(`/api${movieId}/reviews`);
-  // const payload = await response.json();
-  // console.log('Antal: ' + payload.data.length)
+
   const reviews = await getReviews();
-  
-  // Calculate and set parameters for reviewpages
   const reviewsPerPage = 5;
-  const reviewsPages = Math.ceil(reviews.data.length / reviewsPerPage);
+  const totalPages = Math.ceil(reviews.data.length / reviewsPerPage);
   let currentPage = 1;
   
-  // Render the reviews for page nr #
-  const renderReviewPage = async (pageNumber) => {
+  renderReviewPage(reviews, reviewsPerPage, totalPages, currentPage);
+}
 
-    const reviewDiv = document.querySelector("#reviewsList");
-    reviewDiv.innerHTML = "";
-    for(let i = (pageNumber-1) * reviewsPerPage; i < reviewsPerPage*pageNumber; i++) {
-      
-      // Create reviews in DOM
-      if (reviews.data[i]) {
-        const comment = document.createElement('span');
-        comment.classList.add('reviewComment');
-        comment.innerText = reviews.data[i].comment;
-      
-        const rating = document.createElement('span');
-        rating.classList.add('reviewRating');
-        rating.innerText = 'Betyg: ' + reviews.data[i].rating;
+// Render the reviews for page nr #
+const renderReviewPage = async (reviews, reviewsPerPage, totalPages, pageNumber) => {
+
+  const reviewDiv = document.querySelector("#reviewsList");
+  reviewDiv.innerHTML = "";
+
+  // Create reviews in DOM
+  for(let i = (pageNumber-1) * reviewsPerPage; i < reviewsPerPage*pageNumber; i++) {
+    if (reviews.data[i]) {
+      const comment = document.createElement('span');
+      comment.classList.add('reviewComment');
+      comment.innerText = reviews.data[i].comment;
     
-        const author = document.createElement('blockquote');
-        author.classList.add('reviewAuthor');
-        author.innerText = reviews.data[i].author;
-    
-        const li = document.createElement('li');
-        li.append(comment);
-        li.append(rating);
-        li.append(author);
-    
-        reviewDiv.append(li);
-      }
-    }
-
-    // If more than 5 reviews, create pages and buttons to see more reviews
-    if (reviewsPages > 1) {
-      const pages = document.createElement('div');
-      pages.classList.add('reviewPages');
-
-      const prevPage = document.createElement('a');
-      prevPage.classList.add('prevPage');
-      prevPage.innerHTML = '&#10094;';
+      const rating = document.createElement('span');
+      rating.classList.add('reviewRating');
+      rating.innerText = 'Betyg: ' + reviews.data[i].rating;
   
-      const nextPage = document.createElement('a');
-      nextPage.classList.add('nextPage');
-      nextPage.innerHTML = '&#10095;';
+      const author = document.createElement('blockquote');
+      author.classList.add('reviewAuthor');
+      author.innerText = reviews.data[i].author;
   
-      
-      prevPage.addEventListener("click", () => {
-        if (currentPage == 1) {
-          currentPage = reviewsPages;
-        } else {
-        currentPage--;
-        }
-        renderReviewPage(currentPage);
-      });
+      const li = document.createElement('li');
+      li.append(comment);
+      li.append(rating);
+      li.append(author);
   
-      nextPage.addEventListener("click", () => {
-        if (currentPage == reviewsPages) {
-          currentPage = 1; 
-        } else {
-        currentPage++;
-        }
-        renderReviewPage(currentPage);
-      })
-
-      const showPageNumbers = document.createElement('span');
-      showPageNumbers.innerText = `${currentPage}/${reviewsPages}`;
-
-      pages.append(prevPage);
-      pages.append(showPageNumbers);
-      pages.append(nextPage);
-      document.querySelector('#reviewsList').append(pages);
+      reviewDiv.append(li);
     }
   }
-  renderReviewPage(currentPage);
+  // If more than 5 reviews, create pages and buttons to see more reviews
+  if (totalPages > 1) {
+    const pages = document.createElement('div');
+    pages.classList.add('reviewPages');
+
+    const prevPage = document.createElement('a');
+    prevPage.classList.add('prevPage');
+    prevPage.innerHTML = '&#10094;';
+
+    const nextPage = document.createElement('a');
+    nextPage.classList.add('nextPage');
+    nextPage.innerHTML = '&#10095;';
+
+    prevPage.addEventListener("click", () => {
+      if (pageNumber == 1) {
+        pageNumber = totalPages;
+      } else {
+      pageNumber--;
+      }
+      renderReviewPage(reviews, reviewsPerPage, totalPages, pageNumber);
+    });
+
+    nextPage.addEventListener("click", () => {
+      if (pageNumber == totalPages) {
+        pageNumber = 1; 
+      } else {
+      pageNumber++;
+      }
+      renderReviewPage(reviews, reviewsPerPage, totalPages, pageNumber);
+    })
+    
+    const showPageNumbers = document.createElement('span');
+    showPageNumbers.innerText = `${pageNumber}/${totalPages}`;
+
+    pages.append(prevPage);
+    pages.append(showPageNumbers);
+    pages.append(nextPage);
+    document.querySelector('#reviewsList').append(pages);
+  }
 }
