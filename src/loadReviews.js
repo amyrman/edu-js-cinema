d22851509dccc5620cec01b52ce34a7b48dbab6c
Emil Noren <stratosfloor@hotmail.com>
2022-02-03 13:06:06 +0100

    Refactor URL

--- src/loadReviews.js
+++ src/loadReviews.js
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch';
 
 const loadReviews = async (id) => {
-  const API = 'https://lernia-kino-cms.herokuapp.com/api';
-  const reviews = await fetch(`${API}/reviews?filters[movie]=${id}&pagination[pageSize]=100`);
+  const API = `https://lernia-kino-cms.herokuapp.com/api/reviews?filters[movie]=${id}&pagination[pageSize]=100`;
+  const reviews = await fetch(API);
   const data = await reviews.json();
   return data.data;
 }
